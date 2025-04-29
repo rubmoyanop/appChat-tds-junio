d502317793d55ea9458aaa9447c05732ae3cda06
rubrub3 <rubenmoyanopalazon@gmail.com>
2025-04-29 13:47:04 +0200

    WIP #15: Añadido el PoolUsuarios como Singleton para gestionar objetos Usuario y evitar ciclos.

--- src/main/java/umu/tds/appchat/controlador/AppChat.java
+++ src/main/java/umu/tds/appchat/controlador/AppChat.java
@@ -5,13 +5,14 @@ import umu.tds.appchat.modelo.*;
 public enum AppChat {
     INSTANCE;
    public static double COSTE_PREMIUM = 100.0;
-   public static final String DAO_TDS = "um.tds.appchat.persistencia.impl.FactoriaDAO_TDS";
+   public static final String DAO_TDS = "umu.tds.appchat.persistencia.FactoriaDAO_TDS";
    private FactoriaDAO factoriaDAO;
    private UsuarioDAO usuarioDAO;
    private ContactoIndividualDAO contactoIndividualDAO;
    private GrupoDAO grupoDAO;
    private MensajeDAO mensajeDAO;
    private Usuario usuarioActual;
+   
    private AppChat() {
     try {
         factoriaDAO = FactoriaDAO.getInstancia(DAO_TDS);
