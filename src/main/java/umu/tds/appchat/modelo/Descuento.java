acfc512c53759a12a3e15080b3e721033d32b4ca
alexcerezal <alexcerezal2003@gmail.com>
2025-04-29 11:28:31 +0200

    Fix #4: Creadas las clases iniciales con el código básico

---
+++ src/main/java/umu/tds/appchat/modelo/Descuento.java
@@ -0,0 +1,5 @@
+package umu.tds.appchat.modelo;
+
+public interface Descuento {
+    public double getDescuento();
+}
---
+++ src/main/java/umu/tds/appchat/modelo/DescuentoPorMensaje.java
@@ -0,0 +1,8 @@
+package umu.tds.appchat.modelo;
+
+public class DescuentoPorMensaje implements Descuento{
+    @Override
+    public double getDescuento() {
+        return 0.0; 
+    }
+}
