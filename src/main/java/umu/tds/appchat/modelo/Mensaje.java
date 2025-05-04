43f5381a362fbb9995cb8078b2609deccfe035a5
Rubén <rubenmoyanopalazon@gmail.com>
2025-05-04 15:48:08 +0200

    WIP #28: Modificación para getter y setter de id para ahora usarlo en `MensajeDAO_TDS`

--- src/main/java/umu/tds/appchat/modelo/Mensaje.java
+++ src/main/java/umu/tds/appchat/modelo/Mensaje.java
@@ -3,14 +3,24 @@ package umu.tds.appchat.modelo;
 import java.time.LocalDateTime;
 
 public class Mensaje {
+    private int id; 
     private String texto;
     private LocalDateTime fechaHora;
     private TipoMensaje tipo;
 
     public Mensaje(String texto, LocalDateTime fechaHora, TipoMensaje tipo) {
+        this.id = 0; 
         this.texto = texto;
         this.fechaHora = fechaHora;
         this.tipo = tipo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getTexto() {
